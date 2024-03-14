c514ed16db49adc5a46d882687f22ae163a40537
DevJuanRuiz <juanruizreyes@outlook.com>
2024-03-13 20:31:09 -0500

    new

--- src/main/java/polyglot/com/controller/TestController.java
+++ src/main/java/polyglot/com/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
 	@GetMapping("/test")
 	public String TestFirstJob() {
-		return "Welcome to MasterDevops |Tomcat v2.0.0 | KUBERNETES EN AWS, POLYGLOT STUDY 07122023";
+		return "Welcome to MasterDevops |Tomcat v2.0.0 | KUBERNETES EN AWS, POLYGLOT STUDY 13032024";
 	}
 }
